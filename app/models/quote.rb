cb718740f06d03e84e13ab6bf2ac756cf5e4a50d
Steven Fernandez <sfernandez0201@gmail.com>
2022-03-28 18:40:34 -0400

    blah

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,4 +3,6 @@ class Quote < ApplicationRecord
 
   validates :name, presence: true
   after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_update_commit -> { broadcast_update_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes", locals: { quote: self }, target: "quotes" }
 end
